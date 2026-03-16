480d63b0283d5d5a9e852cc63fcf42eba41eb0af
Joshua Bell <joshua.bell.828@gmail.com>
2026-03-15 23:37:07 -0400

    feat(client): update module exports

--- raunch/client/__init__.py
+++ raunch/client/__init__.py
@@ -1,5 +1,25 @@
 # raunch/client/__init__.py
 """Living Library client module - unified interface for local and remote connections."""
 
-# Imports added incrementally as modules are created
-__all__ = []
+from .base import BookClient, PageCallback
+from .models import (
+    Page,
+    BookInfo,
+    CharacterInfo,
+    CharacterPage,
+    ReaderInfo,
+    LibrarianInfo,
+)
+
+__all__ = [
+    # Protocol
+    "BookClient",
+    "PageCallback",
+    # Models
+    "Page",
+    "BookInfo",
+    "CharacterInfo",
+    "CharacterPage",
+    "ReaderInfo",
+    "LibrarianInfo",
+]
